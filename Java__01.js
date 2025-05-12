6bd5b723fa4e237818f3ed0d0d512378d908a5d1
TTuB03ABR <artemdivin650@gmail.com>
2025-05-12 22:22:49 +0300

    Add files via upload

---
+++ Java__01.js
@@ -0,0 +1,12 @@
+'Привет Java!'
+var myThing;
+myThing=5;
+myThing="это строка";
+var numberNine=9;
+var stringNine="9";
+numberNine + numberNine;
+stringNine + stringNine;
+var greeting = "Привет, ";
+var myName = "TTuB03ABR";
+greeting + myName;
+myName.length
